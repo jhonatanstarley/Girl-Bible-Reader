276558b2d4cbe5cb6d03788ec82b65357bbce1ea
jhonatanstarley <42123983+jhonatanstarley@users.noreply.github.com>
2024-11-06 23:02:59 -0300

    Apk Automate

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('biblia365-cache').then(cache => cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/Biblia.36x36.png',
+        '/assets/Biblia.48x48.png',
+        '/assets/Biblia.72x72.png',
+        '/assets/Biblia.96x96.png',
+        '/assets/Biblia.144x144.png',
+        '/assets/Biblia.192x192.png',
+        '/assets/Biblia.256x256.png',
+        '/assets/Biblia.512x512.png'
+      ]))
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  });
+  
